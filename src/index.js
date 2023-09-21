ae1a388b6a18149e99a44551342f473c6c490970
minhhoccode111 <minhhoccode111@gmail.com>
2023-09-21 23:50:35 +0700

    init project with webpack

---
+++ src/index.js
@@ -0,0 +1,25 @@
+console.log('Hello via Bun! in index.js');
+console.log('Hello, World!');
+
+import './js/piece-travails/piece-travails';
+
+import './css/reset.css';
+import './css/header.css';
+import './css/footer.css';
+import './css/style.css';
+
+import './css/piece-travails/aside.css';
+import './css/piece-travails/footer.css';
+import './css/piece-travails/header.css';
+import './css/piece-travails/main.css';
+
+//
+// import Knight from './media/img/knight.svg';
+
+// const body = document.querySelector('body');
+// const div = document.createElement('div');
+// body.appendChild(div);
+
+// const myImage = new Image();
+// myImage.src = Knight;
+// div.appendChild(myImage);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: '.src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    // publicPath: 'dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+};
