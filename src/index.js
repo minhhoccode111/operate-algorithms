80eec01ce327757e6b85f90ccfd53e56ce23b669
minhhoccode111 <minhhoccode111@gmail.com>
2023-09-23 14:21:01 +0700

    files change

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 console.log('Hello via Bun! in index.js');
 console.log('Hello, World!');
 
-import './js/piece-travails/piece-travails';
+import './js/piece-travails/piece-travails.js';
 
+import './css/nav.css';
+import './css/main.css';
+import './css/style.css';
 import './css/reset.css';
 import './css/header.css';
 import './css/footer.css';
-import './css/style.css';
+import './css/buttons.css';
+import './css/responsive.css';
 
-import './css/piece-travails/aside.css';
-import './css/piece-travails/footer.css';
-import './css/piece-travails/header.css';
-import './css/piece-travails/main.css';
+import './css/piece-travails-section/main.css';
+import './css/piece-travails-section/aside.css';
+import './css/piece-travails-section/footer.css';
+import './css/piece-travails-section/header.css';
 
 //
 import Knight from './media/img/knight.svg';
 
 const body = document.querySelector('body');
 
-const myImage = new Image();
-myImage.src = Knight;
-body.appendChild(myImage); //  success
+const button = document.createElement('button');
+button.classList = 'css-button-sliding-to-bottom--rose';
+body.appendChild(button);
+button.textContent = 'Click me!';
